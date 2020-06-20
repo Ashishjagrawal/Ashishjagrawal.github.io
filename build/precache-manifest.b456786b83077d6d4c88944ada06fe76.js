13f7a56d5368c14b0c3ab4eed0b6d43f255c4481
Ashish kumar jagrawal <akjagrawal24@gmail.com>
2020-06-20 08:49:46 +0530

    gh-pages deploy

---
+++ build/precache-manifest.b456786b83077d6d4c88944ada06fe76.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1fb6e90b15539c39ea9163485c989b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57fcd7ff0ca7223920c5",
+    "url": "/static/css/3.cffc9e54.chunk.css"
+  },
+  {
+    "revision": "40bb38238438175e2d15",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "cd51efccc3a2a0599a2e",
+    "url": "/static/css/main.c1d4a8d3.chunk.css"
+  },
+  {
+    "revision": "53b0234cc89dcc466fc0",
+    "url": "/static/js/2.c468bdb5.chunk.js"
+  },
+  {
+    "revision": "57fcd7ff0ca7223920c5",
+    "url": "/static/js/3.880b576a.chunk.js"
+  },
+  {
+    "revision": "40bb38238438175e2d15",
+    "url": "/static/js/4.5e21002c.chunk.js"
+  },
+  {
+    "revision": "cd51efccc3a2a0599a2e",
+    "url": "/static/js/main.88307e88.chunk.js"
+  },
+  {
+    "revision": "8cdddc94b085a5fe2a05",
+    "url": "/static/js/runtime-main.18d1ef41.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  }
+]);
